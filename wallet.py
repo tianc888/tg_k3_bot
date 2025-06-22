57f5034e632913db5ca7ca7181651e1a59efe24d
tianc888 <3826915693@qq.com>
2025-06-23 05:06:00 +0800

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,7 +1,7 @@
 import threading
 
-# 线程安全的简单钱包实现（如用数据库请替换）
 _user_balances = {}
+_usernames = {}  # username -> uid
 _bal_lock = threading.Lock()
 
 def get_user_balance(uid):
@@ -13,8 +13,30 @@ def change_user_balance(uid, amount, reason, detail):
         bal = _user_balances.get(uid, 1000)
         bal += amount
         _user_balances[uid] = bal
-        # 这里可添加日志或记录 reason/detail
 
 def set_user_balance(uid, value):
     with _bal_lock:
         _user_balances[uid] = value
+
+def remember_username(uid, username):
+    if username:
+        with _bal_lock:
+            _usernames[username.lstrip("@")] = uid
+
+def get_uid_by_username(username):
+    with _bal_lock:
+        return _usernames.get(username.lstrip("@"))
+
+async def handle_balance(msg, bot):
+    remember_username(msg.from_user.id, msg.from_user.username)
+    bal = get_user_balance(msg.from_user.id)
+    await msg.reply(f"您的余额为：{bal}")
+
+async def handle_wallet_log(msg, bot):
+    await msg.reply("钱包日志功能暂未实现。")
+
+async def handle_recharge(msg, bot):
+    await msg.reply("充值功能暂未实现，请联系管理员。")
+
+async def handle_withdraw(msg, bot):
+    await msg.reply("提现功能暂未实现，请联系管理员。")
